6d93420598c6a21fd30cb3297491fd675b59b1b7
Anderson Pereira <anderson.pereira@viannasempre.com.br>
2024-10-21 08:17:10 -0300

    Error no Commit

--- obj/Debug/netcoreapp3.0/EXERCICIO14.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.0/EXERCICIO14.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EXERCICIO14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1feb846312e427cff5261931c13426be74bbf954")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca3aba2e13e9cb773b7610a3fa380bd352d9e387")]
 [assembly: System.Reflection.AssemblyProductAttribute("EXERCICIO14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EXERCICIO14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
